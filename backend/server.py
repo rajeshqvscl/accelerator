e18ebfd616dd48f64f9ad50e0c8c07f09c4e34b4
Amar1925 <kumar.amar2003@gmail.com>
2026-07-21 13:39:31 +0530

    refactor: remove permissive CORS header from server response headers

--- backend/server.py
+++ backend/server.py
@@ -353,7 +353,6 @@ class QVSCLHandler(SimpleHTTPRequestHandler):
         self.send_response(status)
         self.send_header("Content-Type", "application/json; charset=utf-8")
         self.send_header("Content-Length", str(len(body)))
-        self.send_header("Access-Control-Allow-Origin", "*")
         self.end_headers()
         self.wfile.write(body)
 
